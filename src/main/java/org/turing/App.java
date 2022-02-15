fb9bf6123e8f95d68fd46b92c50dbd6b677f3fb7
Youcef <tools@youcef.de>
2022-02-15 15:31:16 +0100

    test

--- src/main/java/org/turing/App.java
+++ src/main/java/org/turing/App.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    //test
 
     @Override
     public void start(Stage stage) throws IOException {
